9bd03c3745b5b00b8662d89600ba37f6d0e776ae
Adam Brewer <adam@adambrewer.co.uk>
2020-12-12 21:03:11 +0000

    day 12 -- fun day but second part ended up being a bit hacky

---
+++ test/daytwelve_test.go
@@ -0,0 +1,64 @@
+package test
+
+import (
+	"adventofcode2020/daytwelve"
+	"adventofcode2020/utils"
+	"testing"
+)
+
+func TestDay12Part1Example1(t *testing.T) {
+	input, err := utils.LoadLines("../inputs/day12example.txt")
+	if err != nil {
+		t.Error(err)
+	}
+
+	sut := daytwelve.NewShip(input)
+
+	expected := 25
+
+	sut.Run()
+
+	actual := sut.DistanceFromStart()
+
+	if actual != expected {
+		t.Errorf("expected: %d got %d", expected, actual)
+	}
+}
+
+func TestDay12Part1Exercise(t *testing.T) {
+	input, err := utils.LoadLines("../inputs/day12.txt")
+	if err != nil {
+		t.Error(err)
+	}
+
+	sut := daytwelve.NewShip(input)
+
+	expected := 1687
+	sut.Run()
+
+	actual := sut.DistanceFromStart()
+
+	if actual != expected {
+		t.Errorf("expected: %d got %d", expected, actual)
+	}
+}
+
+func TestDay12Part2Example1(t *testing.T) {
+	input, err := utils.LoadLines("../inputs/day12.txt")
+	if err != nil {
+		t.Error(err)
+	}
+
+	sut := daytwelve.NewShip(input)
+	sut.Part = daytwelve.PART2
+
+	expected := 286
+
+	sut.Run()
+
+	actual := sut.DistanceFromStart()
+
+	if actual != expected {
+		t.Errorf("expected: %d got %d", expected, actual)
+	}
+}
